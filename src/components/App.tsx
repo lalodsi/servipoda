06f7e45ca65b15d08a081f18efa38e0c0b0c6f7d
Luis Eduardo Rodríguez Ramírez <rodriguezramirezluiseduardo0@gmail.com>
2024-07-14 21:44:29 -0600

    Add a box message

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import '../styles/App.scss';
 import CallToActionSection from '../sections/CallToAction';
 import ServicesSection from '../sections/Services';
+import BoxMessageSection from '../sections/BoxMessage';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <CallToActionSection />
       <ServicesSection />
+      <BoxMessageSection />
     </div>
   );
 };
---
+++ src/sections/BoxMessage.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../styles/BoxMessage.scss';
+
+const message = "Ofrecemos personal calificado y tecnología adecuada para reducir costos y tiempos, garantizando un servicio eficiente y confiable."
+
+const BoxMessage = () => {
+  return (
+    <div className='BoxMessageContainer'>
+        <div className="BoxMessage">
+            <p>{message}</p>
+        </div>
+    </div>
+  )
+}
+
+export default BoxMessage
