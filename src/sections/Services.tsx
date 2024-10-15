6ca837bccd1dab3e533d1123b904b96b100336ef
Luis Eduardo Rodríguez Ramírez <rodriguezramirezluiseduardo0@gmail.com>
2024-10-14 22:52:54 -0600

    Reduce images acording screen

--- src/sections/Services.tsx
+++ src/sections/Services.tsx
@@ -3,22 +3,26 @@ import '@styles/Services.scss';
 
 interface service {
   name: string,
-  image: string,
+  image: [string, string],
 }
 
 const SOURCE_LINK = "https://github.com/lalodsi/servipoda/blob/master/src/assets/images/"
 
+const FINAL_LINK = '.jpg?raw=true'
+
 const services: service[] = [
-  { name: 'Poda de árboles y palmeras de cualquier diámetro y altura', image: SOURCE_LINK + 'service1.jpg?raw=true' },
-  { name: 'Derribo de árboles y palmeras de alto riesgo', image: SOURCE_LINK + 'service2.jpg?raw=true' },
-  { name: 'Retiro de residuos (hojas, ramas, troncos)', image: SOURCE_LINK + 'service3.jpg?raw=true' },
-  { name: 'Trámites de poda y derribo ante alcaldías y municipios', image: SOURCE_LINK + 'service4.jpg?raw=true' },
-  { name: 'Fumigación y tratamiento de árboles enfermos', image: SOURCE_LINK + 'service5.jpg?raw=true' },
-  { name: 'Mantenimiento preventivo y correctivo de areas verdes', image: SOURCE_LINK + 'service6.jpg?raw=true' },
+  { name: 'Poda de árboles y palmeras de cualquier diámetro y altura', image: ['service1', 'service1-tiny'] },
+  { name: 'Derribo de árboles y palmeras de alto riesgo', image: ['service2', 'service2-tiny'] },
+  { name: 'Retiro de residuos (hojas, ramas, troncos)', image: ['service3', 'service3-tiny'] },
+  { name: 'Trámites de poda y derribo ante alcaldías y municipios', image: ['service4', 'service4-tiny'] },
+  { name: 'Fumigación y tratamiento de árboles enfermos', image: ['service5', 'service5-tiny'] },
+  { name: 'Mantenimiento preventivo y correctivo de areas verdes', image: ['service6', 'service6-tiny'] },
 ];
 
+const resolveLink = (src: string) => SOURCE_LINK + src + FINAL_LINK
+
 const Services: React.FC = () => {
-  const [selectedService, setSelectedService] = useState<string | null>(null);
+  const [selectedService, setSelectedService] = useState<[string, string] | null>(null);
 
   return (
     <section id='services' className="services">
@@ -31,7 +35,7 @@ const Services: React.FC = () => {
             {services.map(service => (
               <li 
                 key={service.name} 
-                onMouseEnter={() => setSelectedService(service.image)} 
+                onMouseEnter={() => setSelectedService(service.image)}
                 onMouseLeave={() => null}
               >
                 {service.name}
@@ -40,7 +44,14 @@ const Services: React.FC = () => {
           </ul>
         </div>
         <div className="service-image">
-          {selectedService && <img src={selectedService} alt="Servicio" />}
+          {selectedService && 
+            <img
+              src={resolveLink(selectedService[1])}
+              srcSet={resolveLink(selectedService[0]) + " 600w, " + resolveLink(selectedService[1]) + " 1280w"}
+              sizes='(max-width: 600px) 80vw, (max-width: 1200px) 100vw, 250vh'
+              alt="Servicio"
+            />
+          }
         </div>
       </div>
     </section>
