0f4421ed9de6a531d15b2e77742afe9f9de0fe24
Luis Eduardo Rodríguez Ramírez <rodriguezramirezluiseduardo0@gmail.com>
2024-12-29 16:21:02 -0600

    Add icons to cards

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,15 +1,28 @@
+import { ReactNode } from "react";
 import "./styles.scss"
+import { FaWhatsapp } from "react-icons/fa";
 
 const IMAGE = 'https://github.com/lalodsi/servipoda/blob/master/src/assets/images/service1.jpg?raw=true'
 
-const CardComponent = () => {
+export interface CardComponentProps{
+  title: string,
+  img: ReactNode,
+  description: string
+}
+
+const CardComponent: React.FC<CardComponentProps> = (props) => {
+  const {
+    title,
+    img,
+    description
+  } = props
   return (
     <div className='Card'>
         <div className="Card-Image">
-            <img src={IMAGE} alt="" />
+          {img}
         </div>
-        <div className="Card-Title">Este es un título</div>
-        <div className="Card-Description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum</div>
+        <div className="Card-Title">{title}</div>
+        <div className="Card-Description">{description}</div>
     </div>
   )
 }
--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -1,14 +1,27 @@
 import "@styles/Contact.scss"
-import CardComponent from '@components/Card'
+import CardComponent, { CardComponentProps } from '@components/Card'
 import ContactForm from '@components/ContactForm'
+import { FaWhatsapp, FaPhoneAlt  } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+
+const contactTypes: CardComponentProps[] = [
+  { title: 'Teléfono', description: '+52 55 1234 5678', img: (<FaPhoneAlt size={100} />)},
+  { title: 'Correo Electrónico', description: 'brauliosanchez@jardimax.com.mx', img: (<MdEmail size={100} />)},
+  { title: 'WhatsApp', description: '+52 55 1234 5678', img: (<FaWhatsapp size={100} />)},
+]
 
 const ContactSection = () => {
   return (
     <div className='Contact'>
         <div className="Contact-Cards">
-            <CardComponent />
-            <CardComponent />
-            <CardComponent />
+          {
+            contactTypes.map(contactData => (
+            <CardComponent
+              title={contactData.title}
+              description={contactData.description}
+              img={contactData.img}
+            />))
+          }
         </div>
         <div className="Contact-Form">
             <ContactForm />
